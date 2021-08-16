e75f5b7371be7b6fc0207b75f2e5713a51d45dad
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-16 15:24:39 +0100

    Styles

---
+++ lib/styleguides/text_styles.dart
@@ -0,0 +1,40 @@
+import 'package:dating_ui/styleguides/colors.dart';
+import 'package:flutter/material.dart';
+
+final TextStyle headingTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 1.1,
+);
+
+final TextStyle whiteNameTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+final TextStyle whiteSubHeadingTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+);
+
+final TextStyle titleStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 22,
+  fontWeight: FontWeight.w600,
+);
+
+final TextStyle subTitleTextStyle = TextStyle(
+  color: secondaryTextColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w200,
+);
+
+final TextStyle actionMenuStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 5,
+);
