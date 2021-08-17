44439c0c653a3af046a743de326c49bc1a19a247
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-17 11:52:28 +0100

    .

--- lib/widgets/my_info.dart
+++ lib/widgets/my_info.dart
@@ -1,3 +1,4 @@
+import 'package:dating_ui/styleguides/text_styles.dart';
 import 'package:dating_ui/widgets/rounded_image.dart';
 import 'package:flutter/material.dart';
 
@@ -8,10 +9,40 @@ class MyInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           RoundedImage(
             imageUrl: 'assets/images/anne.jpeg',
             size: Size(100, 100),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Roseline, 24',
+                style: whiteNameTextStyle,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icons/location_pin.png',
+                width: 20,
+                color: Colors.white,
+              ),
+              Text(
+                " 82 Kilometers",
+                style: whiteSubHeadingTextStyle,
+              ),
+            ],
           ),
         ],
       ),
